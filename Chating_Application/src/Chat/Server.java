9db681b9a74ab23f02229fbf01cd0883504293cb
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-30 17:41:35 +0530

    Add...

--- Chating_Application/src/Chat/Server.java
+++ Chating_Application/src/Chat/Server.java
@@ -5,5 +5,23 @@ import java.io.*;
 
 public class Server 
 {
+	ServerSocket server;
+	Socket socket;
+	public Server() 
+	{
+		try
+		{
+			server = new ServerSocket(7777);
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
 	
+	public static void main(String[] args) {
+		System.out.println("This is Server... Going to Start Server");
+		new Server();
+	}
 }
+ 
