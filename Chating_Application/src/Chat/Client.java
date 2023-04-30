7eef36a6c5c6a693570e1c86c633a13e87ab6729
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-30 18:04:11 +0530

    Add...

--- Chating_Application/src/Chat/Client.java
+++ Chating_Application/src/Chat/Client.java
@@ -1,6 +1,28 @@
 package Chat;
 
+import java.net.Socket;
+import java.io.*;
+
 public class Client {
+	
+	Socket socket;
+	
+	BufferedReader br;
+	PrintWriter out;
+	
+	public Client() 
+	{
+		try 
+		{
+			System.out.println("Sending Request To Sever...");
+			socket = new Socket("127.0.0.1",7777); 
+			System.out.println("Waiting...");
+		}
+		catch (Exception e) 
+		{
+		
+		}
+	}
 
 	public static void main(String[] args) 
 	{
